43303e6e27a5bb7e65885b213169429ff9a3d075
David Williams <david_williams@us.ibm.com>
2012-07-31 23:00:16 -0400

    migrate to git

--- scriptsMisc/getRelengTools.sh
+++ scriptsMisc/getRelengTools.sh
@@ -1,68 +1,65 @@
 #!/usr/bin/env bash
 
-# it is assumed we are executing this in RELENG_TOOLS or the parent of RELENG_TOOLS
+# it is assumed we are executing this in BUILD_TOOLS or the parent of BUILD_TOOLS
 
 # finds file on users path, before current directory
 # hence, non-production users can set their own values for test machines
 source aggr_properties.shsource
 
-if [ -z ${RELENG_TOOLS} ] ; then
-   RELENG_TOOLS=org.eclipse.simrel.tools
+if [ -z ${BUILD_TOOLS} ] ; then
+   BUILD_TOOLS=org.eclipse.simrel.tools
 fi
 
 # This script file is to help get builds started "fresh", when
-# the ${RELENG_TOOLS} directory already exists on local file system.
-# While it is in the cvs repository in ${RELENG_TOOLS}, it is
+# the ${BUILD_TOOLS} directory already exists on local file system.
+# While it is in the cvs repository in ${BUILD_TOOLS}, it is
 # meant to be executed from the parent directory
-# of ${RELENG_TOOLS} on the file system.
+# of ${BUILD_TOOLS} on the file system.
 
 # export is used, instead of checkout, just to avoid the CVS directories and since this code
 # for a local build, there should never be a need to check it back in to CVS.
 
 # If there is no subdirectory, try going up one directory and looking again (in case we are in it).
-if [ ! -e ${RELENG_TOOLS} ]
+if [ ! -e ${BUILD_TOOLS} ]
 then
 	cd ..
-	if [ ! -e ${RELENG_TOOLS} ]
+	if [ ! -e ${BUILD_TOOLS} ]
 	then	
-         echo "${RELENG_TOOLS} does not exist as sub directory";
+         echo "${BUILD_TOOLS} does not exist as sub directory";
 	     exit 1;
 	fi
 fi
 
 
-# make sure RELENG_TOOLS has been defined and is no zero length, or 
+# make sure BUILD_TOOLS has been defined and is no zero length, or 
 # else following will eval to "rm -fr /*" ... potentially catastrophic
-if [ -z "${RELENG_TOOLS}" ]
+if [ -z "${BUILD_TOOLS}" ]
 then
-   echo "The variable RELENG_TOOLS must be defined to run this script"
+   echo "The variable BUILD_TOOLS must be defined to run this script"
    exit 1;
 fi
-echo "    removing all of ${RELENG_TOOLS} ..."
-rm -fr "${RELENG_TOOLS}"/*
-rm -fr "${RELENG_TOOLS}"/.project
-rm -fr "${RELENG_TOOLS}"/.settings
-rm -fr "${RELENG_TOOLS}"/.classpath
-mkdir -p "${RELENG_TOOLS}"
+echo "    removing all of ${BUILD_TOOLS} ..."
+rm -fr "${BUILD_TOOLS}"/*
+rm -fr "${BUILD_TOOLS}"/.project
+rm -fr "${BUILD_TOOLS}"/.settings
+rm -fr "${BUILD_TOOLS}"/.classpath
+mkdir -p "${BUILD_TOOLS}"
 
-#controltag=david_williams_tempBranch3
-controltag=HEAD
-#echo "    checking out $controltag of ${RELENG_TOOLS} from cvs ..."
+BRANCH_TOOLS=master
+TMPDIR_TOOLS=sbtools
+CGITURL=http://davidw.com/git/
 
-#export CVS_RSH=SSH
-#if [ -z ${CVS_INFO} ] ; then
-#   CVS_INFO=:pserver:anonymous@dev.eclipse.org:
-#fi
+rm ${BRANCH_TOOLS}.zip*
+rm -fr ${BUILD_TOOLS}
+wget --no-verbose -O ${BRANCH_TOOLS}.zip ${CGITURL}/${BUILD_TOOLS}/snapshot/${BRANCH_TOOLS}.zip 2>&1
 
-#cvs -Q -f -d ${CVS_INFO}/cvsroot/callisto  export -d ${RELENG_TOOLS} -r $controltag ${RELENG_TOOLS}
+unzip -o ${BRANCH_TOOLS}.zip -d ${TMPDIR_TOOLS} 
 
-rm master.zip*
-rm -fr ${RELENG_TOOLS}
-wget http://davidw.com/git/org.eclipse.simrel.tools/snapshot/master.zip && unzip -o master.zip -d sbtools && rsync -r sbtools/master/ ${RELENG_TOOLS}
+rsync -r ${TMPDIR_TOOLS}/${BRANCH_TOOLS}/ ${BUILD_TOOLS}
 
 echo "    making sure releng control files are executable and have proper EOL ..."
-dos2unix ${RELENG_TOOLS}/*.sh* ${RELENG_TOOLS}/*.properties ${RELENG_TOOLS}/*.xml >/dev/null 2>>/dev/null
-chmod +x ${RELENG_TOOLS}/*.sh > /dev/null
+dos2unix ${BUILD_TOOLS}/*.sh* ${BUILD_TOOLS}/*.properties ${BUILD_TOOLS}/*.xml >/dev/null 2>>/dev/null
+chmod +x ${BUILD_TOOLS}/*.sh > /dev/null
 echo
 
 
