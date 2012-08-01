a0ff651c8644fff034ef393cc7f59975fbae3065
David Williams <david_williams@us.ibm.com>
2012-08-01 00:20:33 -0400

    migrate to git

--- scriptsMisc/getRelengTools.sh
+++ scriptsMisc/getRelengTools.sh
@@ -6,9 +6,8 @@
 # hence, non-production users can set their own values for test machines
 source aggr_properties.shsource
 
-if [ -z ${BUILD_TOOLS} ] ; then
-   BUILD_TOOLS=org.eclipse.simrel.tools
-fi
+
+BUILD_TOOLS=${BUILD_TOOLS:-org.eclipse.simrel.tools}
 
 # This script file is to help get builds started "fresh", when
 # the ${BUILD_TOOLS} directory already exists on local file system.
@@ -16,71 +15,63 @@ fi
 # meant to be executed from the parent directory
 # of ${BUILD_TOOLS} on the file system.
 
-# export is used, instead of checkout, just to avoid the CVS directories and since this code
-# for a local build, there should never be a need to check it back in to CVS.
-
 # If there is no subdirectory, try going up one directory and looking again (in case we are in it).
+# This is just a sanity check, to see if things are as expected, 
+# and at times may have to be commented out if completely fresh.
 if [ ! -e ${BUILD_TOOLS} ]
 then
-	cd ..
-	if [ ! -e ${BUILD_TOOLS} ]
-	then	
-         echo "${BUILD_TOOLS} does not exist as sub directory";
-	     exit 1;
-	fi
+    cd ..
+    if [ ! -e ${BUILD_TOOLS} ]
+    then	
+        echo "${BUILD_TOOLS} does not exist as sub directory";
+        exit 1;
+    fi
 fi
 
 
 # make sure BUILD_TOOLS has been defined and is no zero length, or 
-# else following will eval to "rm -fr /*" ... potentially catastrophic
+# else following will eval to "rm -fr /*" ... potentially catastrophic!
 if [ -z "${BUILD_TOOLS}" ]
 then
-   echo "The variable BUILD_TOOLS must be defined to run this script"
-   exit 1;
+    echo "The variable BUILD_TOOLS must be defined to run this script"
+    exit 1;
 fi
 echo "    removing all of ${BUILD_TOOLS} ..."
-rm -fr "${BUILD_TOOLS}"/*
-rm -fr "${BUILD_TOOLS}"/.project
-rm -fr "${BUILD_TOOLS}"/.settings
-rm -fr "${BUILD_TOOLS}"/.classpath
+rm -fr ${BUILD_TOOLS}
 mkdir -p "${BUILD_TOOLS}"
 
-BRANCH_TOOLS=master
-TMPDIR_TOOLS=sbtools
-CGITURL=http://davidw.com/git
+BRANCH_TOOLS=${BRANCH_TOOLS:-master}
+TMPDIR_TOOLS=${TMPDIR_TOOLS:-sbtools}
+CGITURL=${CGITURL:-http://davidw.com/git}
 
 rm ${BRANCH_TOOLS}.zip*
-rm -fr ${BUILD_TOOLS}
-wget --no-verbose -O ${BRANCH_TOOLS}.zip ${CGITURL}/${BUILD_TOOLS}/snapshot/${BRANCH_TOOLS}.zip 2>&1
+
+wget  ${CGITURL}/${BUILD_TOOLS}/snapshot/${BRANCH_TOOLS}.zip 2>&1
 RC=$?
 if [[ $RC? != 0 ]] 
 then
-	printf "/n/t%s/t%s/n" "ERROR:" "Failed to get ${BRANCH_TOOLS}.zip from  ${CGITURL}/${BUILD_TOOLS}/snapshot/${BRANCH_TOOLS}.zip"
- exit $RC
+    echo "   ERROR: Failed to get ${BRANCH_TOOLS}.zip from  ${CGITURL}/${BUILD_TOOLS}/snapshot/${BRANCH_TOOLS}.zip"
+    exit $RC
 fi
 
 unzip -o ${BRANCH_TOOLS}.zip -d ${TMPDIR_TOOLS} 
 RC=$?
 if [[ $RC? != 0 ]] 
 then
-	printf "/n/t%s/t%s/n" "ERROR:" "Failed to unzip ${BRANCH_TOOLS}.zip to ${TMPDIR_TOOLS}"
- exit $RC
+    printf "/n/t%s/t%s/n" "ERROR:" "Failed to unzip ${BRANCH_TOOLS}.zip to ${TMPDIR_TOOLS}"
+    exit $RC
 fi
 
 rsync -r ${TMPDIR_TOOLS}/${BRANCH_TOOLS}/ ${BUILD_TOOLS}
 RC=$?
 if [[ $RC? != 0 ]] 
 then
-	printf "/n/t%s/t%s/n" "ERROR:" "Failed to copy ${BUILD_TOOLS} to ${TMPDIR_TOOLS}/${BRANCH_TOOLS}/"
- exit $RC
+    printf "/n/t%s/t%s/n" "ERROR:" "Failed to copy ${BUILD_TOOLS} to ${TMPDIR_TOOLS}/${BRANCH_TOOLS}/"
+    exit $RC
 fi
 
 echo "    making sure releng control files are executable and have proper EOL ..."
 dos2unix ${BUILD_TOOLS}/*.sh* ${BUILD_TOOLS}/*.properties ${BUILD_TOOLS}/*.xml >/dev/null 2>>/dev/null
 chmod +x ${BUILD_TOOLS}/*.sh > /dev/null
 echo
-
-
-
-
 
