72790a0c1c309f59af716d46d9ba9748635c3854
David Williams <david_williams@us.ibm.com>
2012-08-02 23:43:04 -0400

    migrate to git

--- bootstrapScript/getRelengTools.sh
+++ bootstrapScript/getRelengTools.sh
@@ -6,20 +6,59 @@
 # While it is in this source repository in ${BUILD_TOOLS}, it is
 # meant to be executed from the parent directory
 # of ${BUILD_TOOLS} on the file system. If completely fresh 
-# (first time) some "sanity check" code below needs to be commented out.
+# (first time) install, some "sanity check" code below needs 
+# overriden with -f after confirming correct current directory is 
+# correctly set in CC, something like /shared/juno . 
 
 
 # finds file on users path, before current directory
 # hence, non-production users can set their own values for test machines
 source aggr_properties.shsource
 
-# handy to get all variables to log, if needed for debugging
-env
+function usage() 
+{
+    printf "\n\tUsage: %s [-f] [-v] " $(basename $0) >&2
+    printf "\n\t\t%s\t%s" "-f" "Allow fresh creation (confirm correct current directory)." >&2
+    printf "\n\t\t%s\t%s" "-v" "Pring verbose debug info." >&2
+}
+
+
+verboseFlag=false
+fresh=false
+while getopts 'hvf' OPTION
+do
+    case $OPTION in
+        h)    usage
+        exit 1
+        ;;
+        v)    verboseFlag=true
+        ;;
+        f)    fresh=true
+        ;;
+        ?)    usage
+        exit 2
+        ;;
+    esac
+done
+
+# This shift is not required in our particular, current case, 
+# But is a common pattern to leave command line args at correct 
+# point, so we leave it in. 
+shift $(($OPTIND - 1))
+
+
+
+# 'env' is handy to print all env variables to log, 
+# if needed for debugging
+if [[ $verboseFlag ]]
+then
+	env
+fi
 
 BUILD_TOOLS=${BUILD_TOOLS:-org.eclipse.simrel.tools}
 
-# echo current directory for debugging
-echo "PWD: ${PWD}"
+# echo current directory
+echo "Current Directory: ${PWD}"
 
 # If there is no subdirectory, try going up one directory and looking again 
 # (in case we are in it).
@@ -28,14 +67,10 @@ echo "PWD: ${PWD}"
 # are not right. 
 # At times may have to be commented out if completely fresh,
 # after confirming "current directory" is as expected.
-if [ ! -e ${BUILD_TOOLS} ]
+if [ ! $fresh && ! -e ${BUILD_TOOLS} ]
 then
-    cd ..
-    if [ ! -e ${BUILD_TOOLS} ]
-    then	
         echo "${BUILD_TOOLS} does not exist as sub directory";
         exit 1;
-    fi
 fi
 
 # even though we define it above, for safety
@@ -47,8 +82,6 @@ then
     exit 1;
 fi
 
-# echo current directory for debugging
-echo "PWD: ${PWD}"
 
 echo "    removing all of ${BUILD_TOOLS} ..."
 rm -fr ${BUILD_TOOLS}
@@ -58,12 +91,9 @@ BRANCH_TOOLS=${BRANCH_TOOLS:-master}
 TMPDIR_TOOLS=${TMPDIR_TOOLS:-sbtools}
 CGITURL=${CGITURL:-http://git.eclipse.org/c/simrel/}
 
-# echo current directory for debugging
-echo "PWD: ${PWD}"
+
 rm ${BRANCH_TOOLS}.zip*
 
-# echo current directory for debugging
-echo "PWD: ${PWD}"
 
 wget  ${CGITURL}/${BUILD_TOOLS}/snapshot/${BRANCH_TOOLS}.zip 2>&1
 RC=$?
