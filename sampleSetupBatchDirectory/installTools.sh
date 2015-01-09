b3ab9fc166d51db8aa673dd3c99973bab0fc1710
David Williams <david_williams@us.ibm.com>
2015-01-08 21:11:25 -0500

    [releng] fix typo of target name

--- sampleSetupBatchDirectory/installTools.sh
+++ sampleSetupBatchDirectory/installTools.sh
@@ -1,2 +1,2 @@
-ant -Drelease=lunalimited -DaggrPropertyFile=/shared/simrel/lunalimited/org.eclipse.simrel.tools/aggr.properties -f /shared/simrel/lunalimited/org.eclipse.simrel.tools/build.xml installTests
+ant -Drelease=lunalimited -DaggrPropertyFile=/shared/simrel/lunalimited/org.eclipse.simrel.tools/aggr.properties -f /shared/simrel/lunalimited/org.eclipse.simrel.tools/build.xml installAggregatorAndTools 
 
