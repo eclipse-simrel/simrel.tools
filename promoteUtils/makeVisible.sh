3506ad4372710b4ec167f2e88f7877379cea39f8
David Williams <david_williams@us.ibm.com>
2016-06-01 14:39:32 -0400

    Bug 494159 - the EPP packages and SimRel jobs automatic "make visible
    Change-Id: Ic921d8f9c82c0c1f5db308fd962427f4e35c0c25
    Signed-off-by: David Williams <david_williams@us.ibm.com>

--- promoteUtils/makeVisible.sh
+++ promoteUtils/makeVisible.sh
@@ -30,76 +30,111 @@
 
 function usage ()
 {
-  printf "\n\t%s\n" "This utility is to copy the two composte*XX.jars to their final name of composite*.jar." >&2
-  printf "\n\t%s\n" "The first (and only) argument is expected to be the pre-visibility label given to the composite files," >&2
-  printf "\t%s\n\n" "such as M4, RC1, etc." >&2
+  printf "\n\t%s" "This utility, ${0##*/}, is to copy the two composte*XX.jars to their final name of composite*.jar." >&2
+  printf "\n\t\t%s\n" "Example: ${0##*/} 'trainName' 'checkpoint'" >&2
+  printf "\n\t%s" "Both arguments are required." >&2
+  printf "\n\t%s" "'trainName' is the final directory segment of where the composite files reside," >&2
+  printf "\n\t\t%s\n" "such as neon, oxygen, etc." >&2
+  printf "\n\t%s" "'checkpoint' is the pre-visibility label given to the composite files," >&2
+  printf "\n\t\t%s\n" "such as M4, RC1, etc. or simply R for final release." >&2
 }
 
-if [[ ! $# = 1 ]]
-then
-  usage
-  exit 1
-fi
+function changeNamesByCopy ()
+{
 
-LABEL=$1
+  REPO_ROOT=$1
 
-if [[ -z "${LABEL}" ]]
-then
-  # presumably this would occur if ever called from another script, with an error in it.
-  # That is, don't think humans would type ./makevisible.sh "" ?
-  printf "\n\t%s\n" "ERROR: first argument was an empty string?! " >&2
-  usage
-  exit 1
-fi
-REPO_ROOT=/home/data/httpd/download.eclipse.org/releases/neon
+  # be paranoid with sanity checks
+  if [[ -z "${REPO_ROOT}"  ]]
+  then
+    echo -e "\n\t[ERROR] REPO_ROOT must be passed in to this function ${0##*/}"
+    exit 1
+  elif [[ ! -e "${REPO_ROOT}" ]]
+  then
+    echo -e "\n\t[ERROR] REPO_ROOT did not exist!\n\tREPO_ROOT: ${REPO_ROOT}"
+    exit 1
+  else
+    echo -e "\n\t[INFO] REPO_ROOT existed as expected:\n\tREPO_ROOT: ${REPO_ROOT}"
+  fi
 
-# be paranoid with sanity checks
-if [[ ! -e "${REPO_ROOT}" ]]
-then
-  echo -e "\n\t[ERROR] REPO_ROOT did not exist!\n\tREPO_ROOT: ${REPO_ROOT}"
-  exit 1
-else
-  echo -e "\n\t[INFO] REPO_ROOT existed as expected:\n\tREPO_ROOT: ${REPO_ROOT}"
-fi
+  if [[ ! -e "${REPO_ROOT}/compositeArtifacts${CHECKPOINT}.jar" ]]
+  then
+    echo -e "\n\t[ERROR] compositeArtifacts${CHECKPOINT}.jar did not exist in REPO_ROOT!"
+    exit 1
+  fi
+  if [[ ! -e "${REPO_ROOT}/compositeArtifacts${CHECKPOINT}.jar" ]]
+  then
+    echo -e "\n\t[ERROR] compositeContent${CHECKPOINT}.jar did not exist in REPO_ROOT!"
+    exit 1
+  fi
 
-if [[ ! -e "${REPO_ROOT}/compositeArtifacts${LABEL}.jar" ]]
-then
-  echo -e "\n\t[ERROR] compositeArtifacts${LABEL}.jar did not exist in REPO_ROOT!"
-  exit 1
-fi
-if [[ ! -e "${REPO_ROOT}/compositeArtifacts${LABEL}.jar" ]]
-then
-  echo -e "\n\t[ERROR] compositeContent${LABEL}.jar did not exist in REPO_ROOT!"
-  exit 1
-fi
+  # The real work begins here
 
-
-
-cp  --verbose ${REPO_ROOT}/compositeArtifacts${LABEL}.jar ${REPO_ROOT}/compositeArtifacts.jar
-RC=$?
-if [[ $RC != 0 ]]
-then
-  echo -e "\n\t[ERROR] copy returned a non zero return code for compositeArtifacts${LABEL}.jar. RC: $RC"
-  exit $RC
-fi
-cp --verbose ${REPO_ROOT}/compositeContent${LABEL}.jar   ${REPO_ROOT}/compositeContent.jar
-RC=$?
-if [[ $RC != 0 ]]
-then
-  echo -e "\n\t[ERROR] copy returned a non zero return code for compositeContent${LABEL}.jar. RC: $RC"
-  exit $RC
-fi
-
-# This HTML change is rarely used, and can probably
-# eliminate, if ever desired.?
-if [[ -e ${REPO_ROOT}/index${LABEL}.html ]]
-then
-  cp --verbose ${REPO_ROOT}/index${LABEL}.html ${REPO_ROOT}/index.html
+  cp -a --verbose ${REPO_ROOT}/compositeArtifacts${CHECKPOINT}.jar ${REPO_ROOT}/compositeArtifacts.jar
   RC=$?
   if [[ $RC != 0 ]]
   then
-    echo -e "\n\t[ERROR] copy returned a non zero return code for index${LABEL}.html. RC: $RC"
+    echo -e "\n\t[ERROR] copy returned a non zero return code for compositeArtifacts${CHECKPOINT}.jar. RC: $RC"
     exit $RC
   fi
+  cp -a --verbose ${REPO_ROOT}/compositeContent${CHECKPOINT}.jar   ${REPO_ROOT}/compositeContent.jar
+  RC=$?
+  if [[ $RC != 0 ]]
+  then
+    echo -e "\n\t[ERROR] copy returned a non zero return code for compositeContent${CHECKPOINT}.jar. RC: $RC"
+    exit $RC
+  fi
+
+  # This HTML change is rarely used, and can probably
+  # eliminate, if ever desired.?
+  if [[ -e ${REPO_ROOT}/index${CHECKPOINT}.html ]]
+  then
+    cp -a --verbose ${REPO_ROOT}/index${CHECKPOINT}.html ${REPO_ROOT}/index.html
+    RC=$?
+    if [[ $RC != 0 ]]
+    then
+      echo -e "\n\t[ERROR] copy returned a non zero return code for index${CHECKPOINT}.html. RC: $RC"
+      exit $RC
+    fi
+  fi
+
+}
+
+# This is entry point to "main" function
+# We require both arguments, since to provide a default could lead to
+# very bad errors if wrong value of "trainName" was used.
+
+if [[ ! $# = 2 ]]
+then
+  usage
+  exit 1
 fi
+
+TRAIN_NAME=$1
+CHECKPOINT=$2
+
+printf "\n\tArguments to utility were:"
+printf "\n\t\tTRAIN_NAME: ${TRAIN_NAME}"
+printf "\n\t\tCHECKPOINT: ${CHECKPOINT}\n"
+
+if [[ -z "${CHECKPOINT}" || -z "${TRAIN_NAME}" ]]
+then
+  # This would be rare. Equates to something like ./makevisible.sh "" M2
+  # But, just in case. Note that something like ./makevisible "   " M2
+  # is still not handled well.
+  printf "\n\t%s\n" "[ERROR]: one or both required arguments were empty?!\n" >&2
+  usage
+  exit 1
+fi
+
+# Note: we do "Sim Rel repo" first, to avoid a small window of users getting
+# EPP metadata for update, but the Sim Rel repo not being ready.
+# Note: we allow "override" of the repo roots by env. variable to make testing easier.
+
+
+SIM_REPO_ROOT=${SIM_REPO_ROOT:-/home/data/httpd/download.eclipse.org/releases/${TRAIN_NAME}}
+changeNamesByCopy "${SIM_REPO_ROOT}"
+
+EPP_REPO_ROOT=${EPP_REPO_ROOT:-/home/data/httpd/download.eclipse.org/technology/epp/packages/${TRAIN_NAME}}
+changeNamesByCopy "${EPP_REPO_ROOT}"
 
