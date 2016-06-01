7f8a79aba160d2bfdf51ad199d31f00cfd690ce3
David Williams <david_williams@us.ibm.com>
2016-06-01 15:11:54 -0400

    Bug 494159 - try rsync --group
    Change-Id: I289aaaccfc63bbc2c910dd198fe54a87420b1ac0
    Signed-off-by: David Williams <david_williams@us.ibm.com>

--- promoteUtils/makeVisible.sh
+++ promoteUtils/makeVisible.sh
@@ -70,14 +70,14 @@ function changeNamesByCopy ()
 
   # The real work begins here
 
-  cp --preserve=mode,ownership --verbose ${REPO_ROOT}/compositeArtifacts${CHECKPOINT}.jar ${REPO_ROOT}/compositeArtifacts.jar
+  rsync --group --verbose ${REPO_ROOT}/compositeArtifacts${CHECKPOINT}.jar ${REPO_ROOT}/compositeArtifacts.jar
   RC=$?
   if [[ $RC != 0 ]]
   then
     echo -e "\n\t[ERROR] copy returned a non zero return code for compositeArtifacts${CHECKPOINT}.jar. RC: $RC"
     exit $RC
   fi
-  cp --preserve=mode,ownership --verbose ${REPO_ROOT}/compositeContent${CHECKPOINT}.jar   ${REPO_ROOT}/compositeContent.jar
+  rsync --group --verbose ${REPO_ROOT}/compositeContent${CHECKPOINT}.jar   ${REPO_ROOT}/compositeContent.jar
   RC=$?
   if [[ $RC != 0 ]]
   then
@@ -89,7 +89,7 @@ function changeNamesByCopy ()
   # eliminate, if ever desired.?
   if [[ -e ${REPO_ROOT}/index${CHECKPOINT}.html ]]
   then
-    cp --preserve=mode,ownership --verbose ${REPO_ROOT}/index${CHECKPOINT}.html ${REPO_ROOT}/index.html
+    rsync --group --verbose ${REPO_ROOT}/index${CHECKPOINT}.html ${REPO_ROOT}/index.html
     RC=$?
     if [[ $RC != 0 ]]
     then
