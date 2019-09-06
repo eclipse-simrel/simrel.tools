bfd5bc380eddd91d9bfd2d2957f3ab978b5f524b
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2019-09-06 14:56:19 +0200

    Preparations for "offline" creation (without direct file access)
    Change children size attribute dynamically, use "RC2" as final release
    
    Change-Id: I66406fec33719756a5d68f1b099d1f79306d5400
    Signed-off-by: Frederic Gurr <frederic.gurr@eclipse-foundation.org>

--- promoteUtils/writeComposites.sh
+++ promoteUtils/writeComposites.sh
@@ -21,9 +21,12 @@ fi
 repoRoot="/home/data/httpd/download.eclipse.org/releases/${stream}"
 # For testing: if WORKSPACE is not defined, the current directory is used.
 WORKSPACE=${WORKSPACE:-${PWD}}
-#  writeRepoRoot="${PWD}/$stream"
+#writeRepoRoot="${PWD}/$stream"
 # This is the "real" place to write results.
 writeRepoRoot="${repoRoot}"
+
+# TODO: replace with SSH call
+dirs=$(ls -1rd ${repoRoot}/20* | xargs -d '\n' -n 1 basename)
 
 #echo -e "\n[DEBUG] writeRepoRoot: ${writeRepoRoot}"
 mkdir -p "${writeRepoRoot}"
@@ -41,15 +44,13 @@ artifactsCompositeJar="${writeRepoRoot}/${artifactsCompositeName}${checkpoint}.j
 contentCompositeName="compositeContent"
 contentCompositeFile="${templocation}/${contentCompositeName}.xml"
 contentCompositeJar="${writeRepoRoot}/${contentCompositeName}${checkpoint}.jar"
-p2Index="${writeRepoRoot}/p2.index"
 
 
 function writeArtifactsHeader {
   local outfile=$1
   local stream=$2
-  # Note: children size will not always be accurate here, for 'update releases' there can be more, but 
-  # this number is just used to allocate an initial array (or, something) so would be a very minor
-  # performance hit to have to "grow" it. But, could be improved in future.
+  local nChildren=$3
+  local size=$(($nChildren+1))
 
   cat <<EOF > ${outfile}
 <?xml version='1.0' encoding='UTF-8'?>
@@ -60,7 +61,7 @@ function writeArtifactsHeader {
     <property name='p2.compressed' value='true'/>
     <property name='p2.atomic.composite.loading' value='true'/>
   </properties>
-  <children size='4'>
+  <children size='${size}'>
     <child location='../../technology/epp/packages/${stream}/'/>
 EOF
 }
@@ -68,6 +69,9 @@ EOF
 function writeContentHeader {
   local outfile=$1
   local stream=$2
+  local nChildren=$3
+  local size=$((nChildren+1))
+
   cat <<EOG > ${outfile}
 <?xml version='1.0' encoding='UTF-8'?>
 <?compositeMetadataRepository version='1.0.0'?>
@@ -77,88 +81,67 @@ function writeContentHeader {
     <property name='p2.compressed' value='true'/>
     <property name='p2.atomic.composite.loading' value='true'/>
   </properties>
-  <children size='4'>
+  <children size='${size}'>
     <child location='../../technology/epp/packages/${stream}/'/>
 EOG
 }
 
 function writeFooter {
   local outfile=$1
-  printf "%s\n" "  </children>" >> "${outfile}"
-  printf "%s\n" "</repository>" >> "${outfile}"
+  cat <<EOH > "${outfile}"
+  </children>
+</repository>
+EOH
 }
 
 function writeCompositeP2Index {
-  local outfile=$1
-  printf "%s\n" "version=1" > "${outfile}"
-  printf "%s\n" "metadata.repository.factory.order=compositeContent.xml" >> "${outfile}"
-  printf "%s\n" "artifact.repository.factory.order=compositeArtifacts.xml" >> "${outfile}"
+  cat <<EOI > "p2.index"
+version=1
+metadata.repository.factory.order=compositeContent.xml
+artifact.repository.factory.order=compositeArtifacts.xml
+EOI
 }
 
 function writeChildren {
   local outfile=$1
-  local repoRoot=$2
-  local checkpoint=$3
+  local dirs=$2
+  local nChildren=$3
 
-  if [[ ! -d ${repoRoot} ]]; then
-    echo -e "\n[ERROR] repoRoot did not exist when passed into writeChildren." 
-    echo -e "\t${repoRoot}"
-    exit 1
-  fi
-  if [[ -z ${checkpoint} ]]; then
-    echo -e "\n[ERROR] checkpoint was not defined to writeChildren." 
-    echo -e "\t${checkpoint}"
-    exit 1
-  fi
-  # NOTE: we always take the "3 most recent builds" 
-  # EXCEPT when we are doing a "final release". This especially matters for
-  # "update releases" since we then we will eventually have more than 3 "dated directories"
-  # and in that case we want them all, not just most recent 3.
-  # This leads to a *hard assumption* that we name release checkpoints as "R[0-9]", such as 
-  # R0 for initial release of "neon", R1 for "neon.1", etc. Further, if we have a "neon.1a" it 
-  # should be R2 checkpoint (we would have an R0 and an R1 already, so this would be the third "child"
-  # repository. Even after a "1a" release, all subsequent ones also should be one higher than popularly 
-  # named. In other words, the integer (plus one) is taken as the number of <child ... /> elements to create.
-  # For the main stream development, we typically use M[0-9] for milestones, 
-  # and RC[0-9] for Release candidates. But we make no assumption there, we always create 3 child elements.
-  #
-  # We use "20" as a prefix to match for all our child repo directories 
-  # such as "2016, 2017, ...". So, in 80 it years will need some maintenance. :) 
-  # But, otherwise, this cheap heuristic would find existing files such as "composite*" files, 
-  # which would be very wrong.
-  pushd "${repoRoot}" # >/dev/null
-  if [[ $checkpoint =~ ^R([0-9])$ ]]; then
-    nChildren=${BASH_REMATCH[1]}
-    nChildren="$((nChildren + 1))"
-    children=$(ls -1td 20* | head -${nChildren})
-    echo -e "\n\t[INFO] Note that checkpoint, $checkpoint, was found to be a final release"
-    echo -e "\t       and nChildren computed to be $nChildren\n"
-  else
-    children=$(ls -1td 20* | head -3)
-    echo -e "\n\t[INFO] Note that checkpoint, $checkpoint, was NOT found to be a final release"
-    echo -e "\t       so nChildren was assumed to be '3'\n"
-  fi
-  popd # >/dev/null
+  children=$(printf "${dirs}\n" | head -n ${nChildren})
 
-  for child in $children
+  for child in ${children}
   do
-    printf "%s%s%s\n" "    <child location='" $child "' />"  >> ${outfile}
+    printf "%s%s%s\n" "    <child location='" ${child} "' />"  >> ${outfile}
   done
 }
 
 # Main
 
-writeArtifactsHeader "${artifactsCompositeFile}" ${stream}
-writeChildren "${artifactsCompositeFile}" "${repoRoot}" ${checkpoint}
+# NOTE: we always take the "3 most recent builds" EXCEPT when we are doing a "final release". 
+# We assume that RC2 will be the final release, in case of a respin we assume RC2a, RC2b, etc
+#
+# We use "20" as a prefix to match for all our child repo directories 
+if [[ ${checkpoint} =~ ^RC2[a-z]*$ ]]; then
+  nChildren=1
+  echo -e "\n\t[INFO] Checkpoint, ${checkpoint}, was found to be a final release."
+else
+  nChildren=3
+  echo -e "\n\t[INFO] Checkpoint, ${checkpoint}, was NOT found to be a final release."
+fi
+
+writeArtifactsHeader "${artifactsCompositeFile}" ${stream} ${nChildren}
+writeChildren "${artifactsCompositeFile}" "${dirs}" ${nChildren}
 writeFooter "${artifactsCompositeFile}"
 
-writeContentHeader "${contentCompositeFile}" ${stream}
-writeChildren "${contentCompositeFile}" "${repoRoot}" ${checkpoint}
+writeContentHeader "${contentCompositeFile}" ${stream} ${nChildren}
+writeChildren "${contentCompositeFile}" "${dirs}" ${nChildren}
 writeFooter "${contentCompositeFile}"
 
 # We go ahead and re-write the file, since even if it 
 # exists already, we do not know what its contents are.
-writeCompositeP2Index "${p2Index}"
+writeCompositeP2Index
+#FIXME: use SCP
+cp p2.index ${writeRepoRoot}/
 
 zip -q --junk-paths "${artifactsCompositeJar}" "${artifactsCompositeFile}"
 zip -q --junk-paths "${contentCompositeJar}" "${contentCompositeFile}"
@@ -166,4 +149,5 @@ zip -q --junk-paths "${contentCompositeJar}" "${contentCompositeFile}"
 # Since these files are in "workspace", we would not *have* to delete them, 
 # but seems best to, to avoid looking at them. 
 rm ${contentCompositeFile} ${artifactsCompositeFile}
+rm p2.index
 
