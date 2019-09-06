7bd61152d33a54dc06a270766a30d288ac28496a
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2019-09-06 12:56:56 +0200

    Use Heredoc and fix coding style to be in line with the current
    maintainer's preference :P
    
    Change-Id: I865e1df353bbaeacba9310de150204cd4a13f69e

--- promoteUtils/writeComposites.sh
+++ promoteUtils/writeComposites.sh
@@ -1,73 +1,111 @@
 #!/usr/bin/env bash
 
-function writeArtifactsHeader
-{
-  outfile=$1
-  stream=$2
-  printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
-  printf "%s\n" "<?compositeArtifactRepository version='1.0.0'?>" >> ${outfile}
-  printf "%s\n" "<repository name='Eclipse Repository'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>" >> ${outfile}
-  printf "%s\n" "  <properties size='3'>" >> ${outfile}
-  printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
-  printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
-  printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
-  printf "%s\n" "  </properties>" >> ${outfile}
-  # Note: size will not always be accurate here, for 'update releases' there can be more, but 
+stream=$1
+checkpoint=$2
+
+# check input
+
+if [[ -z $stream ]]; then
+  echo -e "\n[ERROR] The variable 'stream' was not passed to the writeComposites script"
+  exit $RC
+fi
+
+if [[ -z $checkpoint ]]; then
+  echo -e "\n[ERROR] The variable 'checkpoint' was not passed to the writeComposites script"
+  exit $RC
+fi
+
+# Normally "writeRepoRoots" is the same as "repoRoots", but might not always be, 
+# plus it is very handy for testing this script not to have to write 
+# to the "production" area.
+repoRoot="/home/data/httpd/download.eclipse.org/releases/${stream}"
+# For testing: if WORKSPACE is not defined, the current directory is used.
+WORKSPACE=${WORKSPACE:-${PWD}}
+#  writeRepoRoot="${PWD}/$stream"
+# This is the "real" place to write results.
+writeRepoRoot="${repoRoot}"
+
+#echo -e "\n[DEBUG] writeRepoRoot: ${writeRepoRoot}"
+mkdir -p "${writeRepoRoot}"
+RC=$?
+if [[ $RC != 0 ]]; then
+  echo -e "\n[ERROR] Could not create directory at ${writeRepoRoot}\n"
+  exit $RC
+fi
+
+#echo -e "[DEBUG] repoRoot: ${repoRoot}\n"
+templocation=${WORKSPACE}
+artifactsCompositeName="compositeArtifacts"
+artifactsCompositeFile="${templocation}/${artifactsCompositeName}.xml"
+artifactsCompositeJar="${writeRepoRoot}/${artifactsCompositeName}${checkpoint}.jar"
+contentCompositeName="compositeContent"
+contentCompositeFile="${templocation}/${contentCompositeName}.xml"
+contentCompositeJar="${writeRepoRoot}/${contentCompositeName}${checkpoint}.jar"
+p2Index="${writeRepoRoot}/p2.index"
+
+
+function writeArtifactsHeader {
+  local outfile=$1
+  local stream=$2
+  # Note: children size will not always be accurate here, for 'update releases' there can be more, but 
   # this number is just used to allocate an initial array (or, something) so would be a very minor
   # performance hit to have to "grow" it. But, could be improved in future.
-  printf "%s\n" "  <children size='4'>" >> ${outfile}
-  printf "%s\n" "     <child location='../../technology/epp/packages/${stream}/'/>" >> ${outfile}
 
-
+  cat <<EOF > ${outfile}
+<?xml version='1.0' encoding='UTF-8'?>
+<?compositeArtifactRepository version='1.0.0'?>
+<repository name='Eclipse Repository' type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>
+  <properties size='3'>
+    <property name='p2.timestamp' value='1313779613118'/>
+    <property name='p2.compressed' value='true'/>
+    <property name='p2.atomic.composite.loading' value='true'/>
+  </properties>
+  <children size='4'>
+    <child location='../../technology/epp/packages/${stream}/'/>
+EOF
 }
 
-function writeContentHeader
-{
-  outfile=$1
-  stream=$2
-  printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
-  printf "%s\n" "<?compositeMetadataRepository version='1.0.0'?>" >> ${outfile}
-  printf "%s\n" "<repository name='Eclipse Repository'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>" >> ${outfile}
-  printf "%s\n" "  <properties size='3'>" >> ${outfile}
-  printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
-  printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
-  printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
-  printf "%s\n" "  </properties>" >> ${outfile}
-  # Note: size will not always be accurate here, for 'update releases' there can be more, but 
-  # this number is just used to allocate an initial array (or, something) so would be a very minor
-  # performance hit to have to "grow" it. But, could be improved in future.  
-  printf "%s\n" "  <children size='4'>" >> ${outfile}
-  printf "%s\n" "     <child location='../../technology/epp/packages/${stream}/'/>" >> ${outfile}
-
+function writeContentHeader {
+  local outfile=$1
+  local stream=$2
+  cat <<EOG > ${outfile}
+<?xml version='1.0' encoding='UTF-8'?>
+<?compositeMetadataRepository version='1.0.0'?>
+<repository name='Eclipse Repository' type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeMetadataRepository' version='1.0.0'>
+  <properties size='3'>
+    <property name='p2.timestamp' value='1313779613118'/>
+    <property name='p2.compressed' value='true'/>
+    <property name='p2.atomic.composite.loading' value='true'/>
+  </properties>
+  <children size='4'>
+    <child location='../../technology/epp/packages/${stream}/'/>
+EOG
 }
 
-function writeFooter
-{
-  outfile=$1
-  printf "%s\n" "  </children>" >> ${outfile}
-  printf "%s\n" "</repository>" >> ${outfile}
+function writeFooter {
+  local outfile=$1
+  printf "%s\n" "  </children>" >> "${outfile}"
+  printf "%s\n" "</repository>" >> "${outfile}"
 }
 
-function writeCompositeP2Index
-{
-  printf "%s\n" "version=1" > "${p2Index}"
-  printf "%s\n" "metadata.repository.factory.order=compositeContent.xml" >> "${p2Index}"
-  printf "%s\n" "artifact.repository.factory.order=compositeArtifacts.xml" >> "${p2Index}"
+function writeCompositeP2Index {
+  local outfile=$1
+  printf "%s\n" "version=1" > "${outfile}"
+  printf "%s\n" "metadata.repository.factory.order=compositeContent.xml" >> "${outfile}"
+  printf "%s\n" "artifact.repository.factory.order=compositeArtifacts.xml" >> "${outfile}"
 }
 
-function writeChildren
-{
-  outfile=$1
-  repoRoot=$2
-  if [[ ! -d ${repoRoot} ]]
-  then
+function writeChildren {
+  local outfile=$1
+  local repoRoot=$2
+  local checkpoint=$3
+
+  if [[ ! -d ${repoRoot} ]]; then
     echo -e "\n[ERROR] repoRoot did not exist when passed into writeChildren." 
     echo -e "\t${repoRoot}"
     exit 1
   fi
-  checkpoint=$3
-  if [[ -z ${checkpoint} ]]
-  then
+  if [[ -z ${checkpoint} ]]; then
     echo -e "\n[ERROR] checkpoint was not defined to writeChildren." 
     echo -e "\t${checkpoint}"
     exit 1
@@ -89,8 +127,7 @@ function writeChildren
   # But, otherwise, this cheap heuristic would find existing files such as "composite*" files, 
   # which would be very wrong.
   pushd "${repoRoot}" # >/dev/null
-  if [[ $checkpoint =~ ^R([0-9])$ ]]
-  then
+  if [[ $checkpoint =~ ^R([0-9])$ ]]; then
     nChildren=${BASH_REMATCH[1]}
     nChildren="$((nChildren + 1))"
     children=$(ls -1td 20* | head -${nChildren})
@@ -107,52 +144,9 @@ function writeChildren
   do
     printf "%s%s%s\n" "    <child location='" $child "' />"  >> ${outfile}
   done
-
 }
 
-stream=$1
-checkpoint=$2
-if [[ -z $stream ]]
-then
-  echo -e "\n[ERROR] The variable 'stream' was not passed to the writeComposites script"
-  exit $RC
-fi
-if [[ -z $checkpoint ]]
-then
-  echo -e "\n[ERROR] The variable 'checkpoint' was not passed to the writeComposites script"
-  exit $RC
-fi
-repoRoot="/home/data/httpd/download.eclipse.org/releases/${stream}"
-# Normally "writeRepoRoots" is the same as "repoRoots", but might not always be, 
-# plus it is very handy for testing this script not to have to write 
-# to the "production" area.
-# similarly, for some testing/debugging cases it is 
-# handiest to execute from current directory, if WORKSPACE
-# is not defined already. Thus, we can leave in this line, since 
-# production builds will always be defined on Hudson. 
-WORKSPACE=${WORKSPACE:-${PWD}}
-#  writeRepoRoot="${PWD}/$stream"
-# This is the "real" place to write results.
-writeRepoRoot="${repoRoot}"
-
-#echo -e "\n[DEBUG] writeRepoRoot: ${writeRepoRoot}"
-mkdir -p "${writeRepoRoot}"
-RC=$?
-if [[ $RC != 0 ]]
-then
-  echo -e "\n[ERROR] Could not create directory at ${writeRepoRoot}\n"
-  exit $RC
-fi
-
-#echo -e "[DEBUG] repoRoot: ${repoRoot}\n"
-templocation=${WORKSPACE}
-artifactsCompositeName="compositeArtifacts"
-artifactsCompositeFile="${templocation}/${artifactsCompositeName}.xml"
-artifactsCompositeJar="${writeRepoRoot}/${artifactsCompositeName}${checkpoint}.jar"
-contentCompositeName="compositeContent"
-contentCompositeFile="${templocation}/${contentCompositeName}.xml"
-contentCompositeJar="${writeRepoRoot}/${contentCompositeName}${checkpoint}.jar"
-p2Index="${writeRepoRoot}/p2.index"
+# Main
 
 writeArtifactsHeader "${artifactsCompositeFile}" ${stream}
 writeChildren "${artifactsCompositeFile}" "${repoRoot}" ${checkpoint}
