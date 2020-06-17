ba8eb214a1d06f305ee7b348fca077f5c3481bf2
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2020-06-17 15:11:29 +0200

    Add usage for update_build_configuration.sh script
    Change-Id: Ifdf5f5eafa15a21356fb13eb74cf86046b7d02e3
    Signed-off-by: Frederic Gurr <frederic.gurr@eclipse-foundation.org>

--- nextReleaseUtils/update_build_configuration.sh
+++ nextReleaseUtils/update_build_configuration.sh
@@ -30,6 +30,35 @@ release_name=${1:-}
 reference_repo=${2:-}
 eclipse_repo_url=${3:-}
 
+usage() {
+  printf "Usage: %s release_name reference_repo eclipse_repo_url\n" "${script_name}"
+  printf "\t%-16s release name (e.g. 2020-09).\n" "project_name"
+  printf "\t%-16s reference repo (e.g. 'releases/2020-06/202006171000').\n" "reference_repo"
+  printf "\t%-16s Eclipse repo URL (e.g. 'https://download.eclipse.org/eclipse/updates/4.16/R-4.16-202006040540/').\n" "eclipse_repo_url"
+}
+
+## Verify inputs
+
+#TODO: check number of arguments
+
+if [ "${release_name}" == "" ]; then
+  printf "ERROR: a release name must be given (e.g. 2020-09).\n"
+  usage
+  exit 1
+fi
+
+if [ "${reference_repo}" == "" ]; then
+  printf "ERROR: a reference repo must be given (e.g. 'releases/2020-06/202006171000').\n"
+  usage
+  exit 1
+fi
+
+if [ "${eclipse_repo_url}" == "" ]; then
+  printf "ERROR: an Eclipse repo URL must be given (e.g. 'https://download.eclipse.org/eclipse/updates/4.16/R-4.16-202006040540/').\n"
+  usage
+  exit 1
+fi
+
 # Update TRAIN_NAME in Jenkinsfile
 sed -i "s/TRAIN_NAME = \".*\"/TRAIN_NAME = \"${release_name}\"/g" ${jenkinsfile}
 
