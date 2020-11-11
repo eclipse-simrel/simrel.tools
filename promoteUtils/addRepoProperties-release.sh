3a0777759f6bd319a4d2064af3a5e7a56ca31ba6
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2020-11-11 18:07:55 +0100

    Clean up addRepoProperties-release.sh script
    Change-Id: Ib9d9240fed4bf9424f71f8c69569ddd016dbc8df
    Signed-off-by: Frederic Gurr <frederic.gurr@eclipse-foundation.org>

--- promoteUtils/addRepoProperties-release.sh
+++ promoteUtils/addRepoProperties-release.sh
@@ -10,25 +10,19 @@
 #     IBM Corporation - initial API and implementation
 #*******************************************************************************
 
+release="${1:-}"
+dirdate="${2:-}"
 
-release=$1
-dirdate=$2
-
-if [ -z "$dirdate" ] ; then
-  echo "ERROR: 'dirdate' directory variable is required for this script.";
+if [ -z "$dirdate" ]; then
+  echo "ERROR: the 'dirdate' directory variable is required for this script.";
   exit 1;
 fi
 
-if [[ -z "${release}" ]]
-then
-  echo
-  echo "   ERROR: The 'release' environment variable is required for this script."
-  echo
+if [[ -z "${release}" ]]; then
+  echo "ERROR: the 'release' environment variable is required for this script."
   exit 1
 else
-  echo
   echo "release: ${release}"
-  echo
 fi
 
 source promote.shsource 2>/dev/null
@@ -39,21 +33,14 @@ APP_NAME=org.eclipse.wtp.releng.tools.addRepoProperties
 devworkspace="${BUILD_HOME}/workspace-addRepoProperties"
 
 echo "dev:           $0"
-echo
-echo "release:       $release"
-echo
-echo "dirdate: $dirdate"
-echo
-echo "devworkspace:   $devworkspace"
-echo
-echo "JAVA_CMD:        $JAVA_CMD"
-echo
-echo "devArgs:       $devArgs"
-echo
-echo "APP_NAME:      $APP_NAME"
+echo "release:       ${release}"
+echo "dirdate:       ${dirdate}"
+echo "devworkspace:  ${devworkspace}"
+echo "JAVA_CMD:      ${JAVA_CMD}"
+echo "devArgs:       ${devArgs}"
+echo "APP_NAME:      ${APP_NAME}"
 $JAVA_CMD -version
 echo
 
-"${ECLIPSE_EXE}"   -debug -nosplash -consolelog -console -data "$devworkspace" --launcher.suppressErrors -application "${APP_NAME}" "${OTHER_ARGS}" -vm "$JAVA_CMD" -vmargs $devArgs
-
+"${ECLIPSE_EXE}" -debug -nosplash -consolelog -console -data "${devworkspace}" --launcher.suppressErrors -application "${APP_NAME}" "${OTHER_ARGS}" -vm "${JAVA_CMD}" -vmargs "${devArgs}"
 
