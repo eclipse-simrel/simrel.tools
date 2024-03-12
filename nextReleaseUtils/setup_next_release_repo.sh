2916c3bd7171a7bc1fd8cf21bd26c7061cb8fc06
Ed Merks <ed.merks@gmail.com>
2024-03-12 15:30:52 +0100

    Improve generated repository composite names.
    For update_latest_repo.sh it should be Eclipse SimRel Latest or Eclipse
    SimRel Milestone.
    
    For setup_next_release_repo.sh it shoudl be Eclipse SimRel 2024-06,
    i.e., the release name
    
    https://github.com/eclipse-simrel/simrel.build/issues/286

--- nextReleaseUtils/setup_next_release_repo.sh
+++ nextReleaseUtils/setup_next_release_repo.sh
@@ -73,7 +73,7 @@ EOF
   cat <<EOG > compositeArtifacts.xml
 <?xml version='1.0' encoding='UTF-8'?>
 <?compositeArtifactRepository version='1.0.0'?>
-<repository name='Eclipse Repository'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>
+<repository name='Eclipse SimRel ${release_name}'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>
   <properties size='3'>
     <property name='p2.timestamp' value='${timestamp}'/>
     <property name='p2.compressed' value='true'/>
@@ -90,7 +90,7 @@ EOG
   cat <<EOH > compositeContent.xml
 <?xml version='1.0' encoding='UTF-8'?>
 <?compositeMetadataRepository version='1.0.0'?>
-<repository name='Eclipse Repository'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>
+<repository name='Eclipse SimRel ${release_name}'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>
   <properties size='3'>
     <property name='p2.timestamp' value='${timestamp}'/>
     <property name='p2.compressed' value='true'/>
--- nextReleaseUtils/update_latest_repo.sh
+++ nextReleaseUtils/update_latest_repo.sh
@@ -65,7 +65,7 @@ EOF
   cat <<EOG > compositeArtifacts.xml
 <?xml version='1.0' encoding='UTF-8'?>
 <?compositeArtifactRepository version='1.0.0'?>
-<repository name='Eclipse Repository'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>
+<repository name='Eclipse SimRel ${local_dir_name^}'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>
   <properties size='3'>
     <property name='p2.timestamp' value='${timestamp}'/>
     <property name='p2.compressed' value='true'/>
@@ -81,7 +81,7 @@ EOG
   cat <<EOH > compositeContent.xml
 <?xml version='1.0' encoding='UTF-8'?>
 <?compositeMetadataRepository version='1.0.0'?>
-<repository name='Eclipse Repository'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>
+<repository name='Eclipse SimRel ${local_dir_name^}'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>
   <properties size='3'>
     <property name='p2.timestamp' value='${timestamp}'/>
     <property name='p2.compressed' value='true'/>
